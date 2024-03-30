212c51f59468a51fe173c5da5c540d432f2a6045
Dash10107 <dakshcjain@gmail.com>
2024-03-30 20:28:58 +0530

    changed pickle version and also changed wicket to int64

--- app.py
+++ app.py
@@ -44,14 +44,16 @@ with col5:
 if st.button('Predict Probability'):
     runs_left = target - score
     balls_left = 120 - (overs*6)
-    wickets = 10 - wickets
+    wickets = int(10 - wickets)
     crr = score/overs
     rrr = (runs_left*6)/balls_left
 
     input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
-    print(input_df)
     result = pipe.predict_proba(input_df)
-    print(result)
+    for column in input_df.columns:
+    # Get the data type of the column
+        data_type = input_df[column].dtype
+        print(f"Column '{column}' - Data Type: {data_type}")
     loss = result[0][0]
     win = result[0][1]
     st.header(batting_team + "- " + str(round(win*100)) + "%")
